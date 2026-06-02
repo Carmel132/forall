4c4fa2a9aefb3d1e8cec2c11a9d2cf57f05c2714
Carmel <carmelbarkaee032708@gmail.com>
2026-06-02 00:31:53 -0400

    [parser] Parse 'by linarith' justification sentinel (MT1)

--- src/parser/src/parser.cpp
+++ src/parser/src/parser.cpp
@@ -811,6 +811,11 @@ std::vector<std::string> Parser::parseJustification() {
         refs.push_back("__ring__");
         return refs;
     }
+    if (check(lexer::TokenKind::KwLinarith)) {
+        advance();
+        refs.push_back("__linarith__");
+        return refs;
+    }
     if (!check(lexer::TokenKind::Identifier)) return refs;
     refs.push_back(std::string{advance().lexeme});
     while (check(lexer::TokenKind::And) || check(lexer::TokenKind::KwWith)) {
